57b248547164c23c2f4eed4094ddf7f4cbbf0554
kowagatte <nnryanp@gmail.com>
2019-12-07 23:14:20 -0600

    added mongodb support and changed the cards to be class consistent

--- HearthArena Scraper.py
+++ HearthArena Scraper.py
@@ -1,26 +1,44 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import pymongo
 
-print('--== [ Requesting Info from HearthStoneArena ] ==--')
-page = requests.get("https://www.heartharena.com/tierlist")
-print('DONE')
+client = pymongo.MongoClient("code")
+db = client.HearthArena
 
-print('--== [ Parsing HearthStoneArena HTML ] ==--')
-soup = BeautifulSoup(page.text, 'html.parser')
-print('DONE')
+classes = [{'name':"druid", 'db': db.druid, 'cards': []},
+    {'name':"hunter", 'db': db.hunter, 'cards': []},
+    {'name':"mage", 'db': db.mage, 'cards': []},
+    {'name':"paladin", 'db': db.paladin, 'cards': []},
+    {'name':"priest", 'db': db.priest, 'cards': []},
+    {'name':"rogue", 'db': db.rogue, 'cards': []},
+    {'name':"shaman", 'db': db.shaman, 'cards': []},
+    {'name':"warlock", 'db': db.warlock, 'cards': []},
+    {'name':"warrior", 'db': db.warrior, 'cards': []}]
 
-def downloadHearthStoneArenaPage():
-    file = open("hearthstonearena.html", mode= 'w+', encoding='utf-8')
-    file.write(page.text)
-    file.close()
+def requestHearthArenaData():
+    print('--== [ Requesting Info from HearthStoneArena ] ==--')
+    page = requests.get("https://www.heartharena.com/tierlist")
+    print('DONE')
+    return page
 
-raw_cards = []
-cards = []
+def parseHTML(page):
+    print('--== [ Parsing HearthStoneArena HTML ] ==--')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    print('DONE')
+    return soup
 
-def compileCards():
+def getClassHTML(haHTML, hclass):
+    print('--== [ Getting', hclass, ' html ] ==--')
+    html = haHTML.find("section", {"id":hclass})
+    print('DONE')
+    return html
+    
+
+def compileRawCards(haHTML):
+    raw_cards = []
     print('--== [ Finding Cards ] ==--')
-    category = soup.find_all("ol", {"class":"cards"})
+    category = haHTML.find_all("ol", {"class":"cards"})
     print('DONE')
     print('--== [ Adding Raw Cards ] ==--')
     count = 0
@@ -29,10 +47,10 @@ def compileCards():
         count += 1
         print(round(((count/len(category)) * 100), 3), "percent complete!")
     print("DONE")
-    
-compileCards()
+    return raw_cards
 
-def cleanCards():
+def cleanCards(raw_cards):
+    cards = []
     print('--== [ Cleaning Up Cards ] ==--')
     count = 0
     for raw_card in raw_cards:
@@ -46,15 +64,47 @@ def cleanCards():
         count += 1
         print(round(((count/len(raw_cards)) * 100), 3), "percent complete!")
     print("DONE")
-    
-cleanCards()
+    return cards
 
-def downloadCardData():
+def downloadCardData(cards):
     print('--== [ Writing cards to file ] ==--')
     file = open("arenaCards.json", mode='w+', encoding='utf-8')
     file.write(json.dumps(cards))
     file.close()
     print('DONE')
 
-downloadCardData()
+def downloadHearthStoneArenaPage():
+    file = open("hearthstonearena.html", mode= 'w+', encoding='utf-8')
+    file.write(page.text)
+    file.close()
 
+def doesDataExist():
+    try:                                                                                                                                        
+        file = open("arenaCards.json")
+        file.close()
+        return True
+    except IOError:
+        return False
+
+def compileCardsByClass():
+    page = requestHearthArenaData()
+    html = parseHTML(page)
+    for hclass in classes:
+        hclass['cards'] = cleanCards(compileRawCards(getClassHTML(html, hclass['name'])))
+
+def uploadCardsToDatabase():
+    print('--== [ Writing cards to database ] ==--')
+    for hclass in classes:
+        print('-= [ Saving', hclass['name'], 'cards ] =-')
+        count = 0
+        for card in hclass['cards']:
+            hclass['db'].insert(card)
+            count += 1
+            print(round(((count/len(hclass['cards'])) * 100), 3), "percent complete!")
+        print('DONE')
+    print('DONE')
+    count = 0
+
+
+compileCardsByClass()
+uploadCardsToDatabase()
